95e5c6906ce54923d1dad5a0c81ae033d66ee69d
JackE <jacke@kavanu.co>
2020-08-21 19:31:37 +0300

    language selected as only Turkish - creating a results script to start extracting data for analyzation. Updated gitignore

--- twitterapi.py
+++ twitterapi.py
@@ -115,7 +115,7 @@ class StdOutListener(StreamListener):
 if __name__ == '__main__':
     # Authenticate using config.py and connect to Twitter Streaming API.
     tracking_List = ["yahudi", "yahudiler", "musevi", "museviler", "sinagog", "havra", "haham"]
-    fetched_tweets_filename = str(datetime.date.today())
+    fetched_tweets_filename = str("results")
 
     twitter_streamer = TwitterStreamer()
     twitter_streamer.stream_tweets(fetched_tweets_filename, tracking_List)
