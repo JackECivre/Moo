bd4a658f51959499f1d402d91cd492d862904853
JackE <jacke@kavanu.co>
2021-05-24 00:29:49 +0300

    pandas csv data problem

--- results.py
+++ results.py
@@ -8,38 +8,60 @@ import csv
 
 def results():
 
-    def get_folder_location():
-        selected_location = ""
+    # def get_folder_location():
+    #     selected_location = ""
+    #     try:
+    #         filename = askdirectory()
+    #         selected_location = filename
+    #         print("File Selected as " + selected_location)
+    #     except Exception as Error:
+    #         print(Error)
+    #     return selected_location
+
+    def get_certain_file():
+        selected_file = ""
         try:
-            filename = askdirectory()
-            selected_location = filename
-            print("File Selected as " + selected_location)
+            filename = askopenfilename()
+            selected_file = filename
+            print("File Selected as " + selected_file)
         except Exception as Error:
             print(Error)
-        return selected_location
+        return selected_file
 
-    file_location = get_folder_location()
+    selected_file = get_certain_file()
+    # file_location = get_folder_location()
 
-    def get_all_files(file_location):
+    def read_csv(selected_file):
         try:
-            print("files gathered from " + file_location)
-            all_files = glob.glob(os.path.join(file_location, "*.csv"))
-            all_df = []
-            for f in all_files:
-                df = pd.read_csv(f, sep=',', error_bad_lines=False, delimiter=None, header=1, lineterminator='\n' )
-                df["file"] = f.split(',')[-1]
-                all_df.append(df)
-            merged_df = pd.concat(all_df, ignore_index=True, sort=True)
-            print(merged_df)
-            merged_df.to_csv("merged.csv")
-            print("MERGED CSV FILE CREATED")
+            moo_data = pd.read_csv(rf"{selected_file}",error_bad_lines = False, sep=",", lineterminator = '\n')
+            print(moo_data.head())
         except Exception as Error:
-            print("Merge CSV Error: ")
+            print("pandas CSV Read Error: ")
             print(Error)
 
+    read_csv(selected_file)
 
-
-    get_all_files(file_location)
+    # def merge_csv_files(file_location):
+    #     try:
+    #         print("files gathered from " + file_location)
+    #         all_files = glob.glob(os.path.join(file_location, "*.csv"))
+    #         all_df = []
+    #         print(all_files)
+    #         for f in all_files:
+    #             df = pd.read_csv(f, sep=',', error_bad_lines=False, delimiter=",", header=1, lineterminator='\n' )
+    #             df["file"] = f.split(',')[-1]
+    #             all_df.append(df)
+    #         merged_df = pd.concat(all_df, ignore_index=True, sort=True)
+    #         print(merged_df)
+    #         merged_df.to_csv("merged.csv")
+    #         print("MERGED CSV FILE CREATED")
+    #     except Exception as Error:
+    #         print("Merge CSV Error: ")
+    #         print(Error)
+    #
+    #
+    #
+    # merge_csv_files(file_location)
 
     # filename_excel = get_folder_location().replace(".csv", ".xlsx")
 
